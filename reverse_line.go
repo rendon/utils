2e10a08a8106999ea4bc384ffcf521d6f5f24084
Rafael Rendon Pablo <rafaelrendonpablo@gmail.com>
2017-01-31 09:57:30 -0800

    Add reverse_line command

---
+++ reverse_line.go
@@ -0,0 +1,27 @@
+// Utility to reverse lines in a file
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadBytes('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read line: %s", err)
+		os.Exit(1)
+	}
+	i := 0
+	j := len(line) - 1 - 1 // Extra -1 to exclude delimiter
+	for i < j {
+		t := line[i]
+		line[i] = line[j]
+		line[j] = t
+		i++
+		j--
+	}
+	fmt.Printf("%s", line)
+}
